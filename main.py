dc19bede41986a00f2cde26ad3d3ed66ff361333
Winglet-Hell <i@stpra.ru>
2024-07-31 19:55:16 +0300

    python-homework

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ average_time_per_homework = hours_spent / num_homeworks
 
 print("Курс:", course_name, "всего задач:", num_homeworks, "затрачено часов:", hours_spent, "среднее время выполнения", average_time_per_homework,"часа.")
 
-
+# Вариант с f.
 print(f"Курс: {course_name}, всего задач: {num_homeworks}, затрачено часов: {hours_spent}, среднее время выполнения {average_time_per_homework} часа.")
 
-# В курсе не сказано про использование "f". Знал о нем из своего опыта. Поэтому также сделал строку "вручную"
+# Вариант только со str.
 print("Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.")
