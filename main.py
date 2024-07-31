c24c31230a7f370b1f98317f7ea9f98b9489b5ca
Winglet-Hell <i@stpra.ru>
2024-07-31 19:53:50 +0300

    python-homework

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ hours_spent = 1.5
 course_name = 'Python'
 average_time_per_homework = hours_spent / num_homeworks
 
+print("Курс:", course_name, "всего задач:", num_homeworks, "затрачено часов:", hours_spent, "среднее время выполнения", average_time_per_homework,"часа.")
+
+
 print(f"Курс: {course_name}, всего задач: {num_homeworks}, затрачено часов: {hours_spent}, среднее время выполнения {average_time_per_homework} часа.")
 
 # В курсе не сказано про использование "f". Знал о нем из своего опыта. Поэтому также сделал строку "вручную"
